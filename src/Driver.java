0d8cb524d43be7245e301c1058c4baa9b9de8e57
khaledjabr <khaled.n.jabr@gmail.com>
2016-10-15 18:35:05 -0500

    finished section 2

--- src/Driver.java
+++ src/Driver.java
@@ -124,20 +124,66 @@ public class Driver {
 	}
 
 	
-	public static void insertPerformer2(int pid, String name, int age , int did) 
+	public static void insertPerformer2(int pid, String pname, int age , int did) 
 	{ 
-		System.out.println(pid +" " + name +" " + age +" "+ did);
+		System.out.println(pid +" " + pname +" " + age +" "+ did);
 		
-		try {
-			stmt = conn.createStatement();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("ERROR in statement");
-		} 
-		
+		int years_of_experience = 0; 
 		//find average age of  all performers who have acted in a movie that was directed by a director with a given did
 	
 		//find all these performers 
+		
+		String peformersQuery = "SELECT DISTINCT pname, years_of_experience FROM performer,acted WHERE performer.pid  =acted.pid AND mname IN (SELECT mname FROM movie, director WHERE movie.did = director.did AND director.did = 2)";
+		try {
+			ResultSet performers = stmt.executeQuery(peformersQuery);
+			
+			int numberOfPerformers = 0 ; 
+			int sumOfYears = 0; 
+			while(performers.next())
+			{
+				System.out.println(performers.getString(1) + "   " + performers.getString(2));
+				numberOfPerformers++; 
+				sumOfYears += Integer.parseInt(performers.getString(2));
+			}
+			
+			if(numberOfPerformers == 0) 
+			{
+				years_of_experience =  age - 18; 
+				
+				if(years_of_experience < 0)
+						years_of_experience = 0 ;
+			}
+			else { 
+				int average_years =  sumOfYears / numberOfPerformers; 
+				if(average_years > age )
+					years_of_experience = age;
+				else 
+					years_of_experience = average_years;  
+				
+				
+			}
+			
+			//Insert to table 
+			
+			System.out.println("years of experience: " + years_of_experience);
+			
+			String sqlInsert = "insert into performer values(" 
+					+ pid 
+					+ ", '"
+					+ pname
+					+ "', "
+					+ years_of_experience
+					+ ", "
+					+ age
+					+ ")";
+			
+			stmt.executeQuery(sqlInsert);
+			
+			
+		} catch (SQLException e) {
+			System.out.println("SQL EXCEPTION : " + e.getMessage());
+			System.exit(1);
+		}
 		
 		
 	}
