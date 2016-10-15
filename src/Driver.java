666ff457ef787182acd64cd8d8932690d95f9bcb
Brian Neldon <brian.neldon@gmail.com>
2016-10-15 18:04:52 -0500

    hopefully option 1 works

--- src/Driver.java
+++ src/Driver.java
@@ -9,7 +9,6 @@ public class Driver {
 	public static void main(String[] args) {
 		// Step 1. Loading a database driver 
 		String sourceURL = "jdbc:oracle:thin:@//oracle.cs.ou.edu:1521/pdborcl.cs.ou.edu";
-		int option;
 		int pid;
 		String pname;
 		int age;
@@ -41,7 +40,7 @@ public class Driver {
 			    	}
 			    	else if (args.length >= 4){
 			    		did = Integer.parseInt(args[3]);
-			    		insertPerformer2();
+			    		//insertPerformer2();
 			    	}
 			        
 			    } catch (NumberFormatException e) {
@@ -62,25 +61,59 @@ public class Driver {
 	}
 	
 	public static boolean insertPerformer1(int pid, String pname, int age){
-
-//		String sqlInsert = "insert into performer values(" 
-//				+ pid 
-//				+ ", '"
-//				+ pname
-//				+ "', "
-//				+ years_of_experience
-//				+ ","
-//				+ age
-//				+ ")";
+		
+		int lower = (age - 10 >= 0) ? age - 10 : 0;
+		int upper = age + 10;
+		int counter = 0;
+		int totalYears = 0;
+		int years_of_experience;
+		String experienceSql = "select years_of_experience from performer where age between "
+				+ lower + "and " + upper;
+		
+		//find years of experience of performers <= 10 years younger or older
+		try {
+			ResultSet result = stmt.executeQuery(experienceSql);
+			while(result.next()) {
+				System.out.println(result.getString(1)); 
+				try {
+					totalYears += Integer.parseInt(result.getString(1));
+					counter++;
+				} catch(NumberFormatException e) {
+					System.err.println("Uh oh, that wasn't a number!");
+			        System.exit(1); 
+				}
+			}
+		} catch(SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+			System.exit(1);
+		} 
+		
+		if(counter == 0){
+				years_of_experience = (age - 18 >= 0) ? age - 18 : 0;
+		} else {
+			years_of_experience = (int) totalYears / counter;
+		}	
+		System.out.println("years of experience: " + years_of_experience);
+		
+		String sqlInsert = "insert into performer values(" 
+				+ pid 
+				+ ", '"
+				+ pname
+				+ "', "
+				+ years_of_experience
+				+ ", "
+				+ age
+				+ ")";
 		
 		try {
-			//stmt.executeUpdate(sqlInsert);
+			stmt.executeUpdate(sqlInsert);
 			ResultSet result = stmt.executeQuery("select * from performer");
-			System.out.println("PerformerID      Name ");
-			while(result.next())
-			{
-				System.out.println(result.getString(1) + "      " + result.getString(2)); 
-				
+			while(result.next()) {
+				System.out.println("PID Name Experience Age");
+				System.out.println(result.getString(1)
+						+ " " + result.getString(2)
+						+ " " + result.getString(3)
+						+ " " + result.getString(4)); 
 			}
 		} catch(SQLException ex) {
 			System.err.println("SQLException: " + ex.getMessage());
